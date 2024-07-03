1dc27b3d21ac47631024b58ae4fa95831205fe96
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2024-07-03 02:10:41 -0400

    tiempos por usuario logueado 2

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,49 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import db from "../../../../libs/db";
 import bcrypt from "bcrypt";
 
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password", placeholder: "*****" },
+      },
+      async authorize(credentials, req) {
+        const userFound = await db.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!userFound) throw new Error("No user found");
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+
+        if (!matchPassword) throw new Error("Wrong password");
+
+        return {
+          id: userFound.id,
+          name: userFound.username,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+
+export const GET = (req, res) => NextAuth(req, res, authOptions);
+export const POST = (req, res) => NextAuth(req, res, authOptions);
+
+/* import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import db from "../../../../libs/db";
+import bcrypt from "bcrypt";
+
 export const authOptions = {
   providers: [
     CredentialsProvider({
@@ -41,3 +84,4 @@ export const authOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+ */
