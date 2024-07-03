8813084db2de3286ff42cb73aa6548ad5bac3413
Nicolás Carrasco <nicolas.carrasco.m@gmail.com>
2024-07-03 01:50:33 -0400

    tiempos por usuario logueado

--- src/app/api/swimmers/[id]/route.ts
+++ src/app/api/swimmers/[id]/route.ts
@@ -1,4 +1,4 @@
-import prisma from "@/lib/prisma";
+import prisma from "@/libs/db";
 import { swimmers } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
 import * as yup from "yup";
